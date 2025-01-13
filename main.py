0be33c99be47fa8b274691c0b0ac4c1a7e06cdf7
Catalin Tirdea <tirdea.catalin@yahoo.com>
2025-01-14 01:06:20 +0200

    l-am facut sa mearga, urmeaza sa il optimizez, acuratetea este undeva intre 0.54 si 0.57

--- main.py
+++ main.py
@@ -1,68 +1,72 @@
-import numpy as np
-import pandas as pd
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
+import numpy as np
+import pandas as pd
 from llm2vec import LLM2Vec
-from sklearn.linear_model import LogisticRegression
 from transformers import AutoModel, AutoTokenizer
 
-# Defineste modelul si tokenizer-ul 
+# Initializează tokenizer și model
 model_name = "bert-base-uncased"
 model = AutoModel.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-# Configureaza tokenizer-ul sa foloseasca padding pe partea stanga (Important pentru LLM2Vec)
 tokenizer.padding_side = 'left'
 
-# Incarca datele
-data = pd.read_csv('./bold_response_LH.csv')  # citirea datelor folosind pandas
-sentences = data['sentence']  # Coloana cu propozitii
-
-# Definirea coloanelor care vor fi prezise
+# Încarcă datele
+data = pd.read_csv('./bold_response_LH.csv')
+sentences = data['sentence']
 columns_to_predict = [
     'lang_LH_AntTemp', 'lang_LH_IFG', 'lang_LH_IFGorb', 
     'lang_LH_MFG', 'lang_LH_PostTemp', 'lang_LH_netw'
 ]
 
-# Initializeaza LLM2Vec
+# Discretizează coloanele de predicție
+for column in columns_to_predict:
+    data[column] = (data[column] > data[column].median()).astype(int)
+
+# Generează vectori
 llm2vec = LLM2Vec(model=model, tokenizer=tokenizer)
 
-# Dimensiunea fixa la care dorim sa completam sau trunchiem vectorii
-fixed_dimension = 768
-
-# Genereaza vectori pentru fiecare propozitie
-sentence_vectors = []
-for sentence in sentences:
+def encode_sentence(sentence):
     vector = llm2vec.encode(sentence)
-    # Asigura-te ca vectorul are dimensiunea corecta
-    if len(vector) > fixed_dimension:
-        vector = vector[:fixed_dimension]  # Trunchiem vectorul
-    elif len(vector) < fixed_dimension:
-        # Daca vectorul este mai mic, completam cu 0
-        vector = np.pad(vector, (0, fixed_dimension - len(vector)), 'constant')
-    sentence_vectors.append(vector)
+    if len(vector.shape) == 2:
+        vector = vector.detach().numpy()
+        vector = np.mean(vector, axis=0)
+    return vector
 
-# Convertim lista de vectori intr-un numpy array omogen
-#TODO: Eroare aici, trebuie sa se converteasca in numpy array
-sentence_vectors = np.array(sentence_vectors)
+sentence_vectors = np.array([encode_sentence(sentence) for sentence in sentences])
 
-# Initializeaza modelul de predictie
+# Initializează modelul de clasificare
 model = LogisticRegression()
 
-# Realizeaza 5-fold cross-validation pentru fiecare coloana ce trebuie prezisa
+# DataFrame pentru salvarea predicțiilor
+predictions_df = pd.DataFrame(sentences, columns=['sentence'])
+
+# 5-fold cross-validation și salvarea predicțiilor
 for column in columns_to_predict:
-    labels = data[column]  # Coloana de etichete pentru fiecare model
+    labels = data[column]
     kf = KFold(n_splits=5)
-    accuracies = []
+    fold_predictions = []
+    accuracies = []  # Mutat în interiorul buclei pentru fiecare coloană
 
     for train_index, test_index in kf.split(sentence_vectors):
         X_train, X_test = sentence_vectors[train_index], sentence_vectors[test_index]
-        y_train, y_test = labels[train_index], labels[test_index]
+        y_train, y_test = labels.iloc[train_index], labels.iloc[test_index]
         
         model.fit(X_train, y_train)
         predictions = model.predict(X_test)
+        
+        # Salvăm predicțiile pentru fold-ul curent
+        fold_predictions.extend(predictions)
+        
+        # Calculăm acuratețea pentru acest fold
         accuracy = accuracy_score(y_test, predictions)
         accuracies.append(accuracy)
 
-    # Afiseaza rezultatele pentru fiecare coloana
+    # Adaugăm predicțiile pentru această coloană în DataFrame
+    predictions_df[column] = fold_predictions
     print(f'Accuracy for predicting {column}: {np.mean(accuracies)}')
+
+# Salvăm predicțiile într-un CSV
+predictions_df.to_csv('predictions.csv', index=False)
+print("Predicțiile au fost salvate în 'predictions.csv'.")
